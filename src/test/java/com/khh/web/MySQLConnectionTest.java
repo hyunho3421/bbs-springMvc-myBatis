9507ccb9f856a95b000729ca975086765082d4f2
haha9006@gmail.com <rlagusgh3421>
2017-05-06 11:36:26 +0900

    db password 변경

--- src/test/java/com/khh/web/MySQLConnectionTest.java
+++ src/test/java/com/khh/web/MySQLConnectionTest.java
@@ -12,7 +12,7 @@ public class MySQLConnectionTest {
     private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/bbs-springMvc-MyBatis";
     private static final String USER = "root";
-    private static final String PW    = "1234";
+    private static final String PW    = "1111";
 
     @Test
     public void testConnection() throws Exception{
